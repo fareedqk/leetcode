f83b11c5530556bc63c86ad1447bf8fb82dc626b
Fareed Khan <115370893+fareedqk@users.noreply.github.com>
2024-06-07 20:18:24 +0000

    Sync LeetCode submission - Find Words Containing Character (python3)

---
+++ my-folder/problems/find_words_containing_character/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        arr = []
+
+        for i in range(len(words)):
+            if x in words[i]:
+                # print(arr)
+                arr.append(i)
+        return arr
