ebbafc11eb4e121fddcee4eb18bb5277ca537377
Fareed Khan <115370893+fareedqk@users.noreply.github.com>
2024-07-12 04:29:51 +0000

    Sync LeetCode submission - Group Anagrams (python3)

--- my-folder/problems/group_anagrams/solution.py
+++ my-folder/problems/group_anagrams/solution.py
@@ -1,16 +1,17 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = defaultdict(list)
+        # res = defaultdict(list)
 
-        for word in strs:
-            sorted_word = "".join(sorted(word))
-            res[sorted_word].append(word)
+        # for word in strs:
+        #     sorted_word = "".join(sorted(word))
+        #     res[sorted_word].append(word)
 
-        # for s in strs:
-        #     count = [0] * 26
-        #     for i in s:
-        #         count[ord(i) - ord('a')] += 1
-
-        #     res[tuple(count)].append(s)
-
-        return res.values()
+        # return res.values()
+        res = {}
+        for s in strs:
+            f = str(sorted(s))
+            if f not in res:
+                res[f] = []
+            res[f].append(s)
+        
+        return list(res.values())
