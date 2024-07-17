eb3d4adf7b09034f69b47fac8d93d6855136539e
Fareed Khan <115370893+fareedqk@users.noreply.github.com>
2024-07-17 09:31:07 +0000

    Sync LeetCode submission - Valid Anagram (python3)

--- my-folder/problems/valid_anagram/solution.py
+++ my-folder/problems/valid_anagram/solution.py
@@ -1,3 +1,14 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        if len(s) != len(t):
+            return False
+    
+        count_s, count_t = {}, {}
+
+        # creating hash maps
+        for i in range(len(s)):
+            count_s[s[i]] = 1 + count_s.get(s[i], 0)
+            count_t[t[i]] = 1 + count_t.get(t[i], 0)
+        # checking if the keys and values are same
+        # for c in count_s:
+        return count_s == count_t
