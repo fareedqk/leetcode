bc2c9f125b093dc2f1c1cc1460f34d48a557f0cd
Fareed Khan <115370893+fareedqk@users.noreply.github.com>
2024-07-20 13:51:07 +0000

    Sync LeetCode submission - Valid Palindrome (python3)

--- my-folder/problems/valid_palindrome/solution.py
+++ my-folder/problems/valid_palindrome/solution.py
@@ -1,9 +1,20 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        new = ""
+        l, r = 0, len(s)-1
+        while l < r:
+            while l < r and not self.alphaNum(s[l]):
+                l += 1
+            while l < r and not self.alphaNum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l, r = l + 1, r - 1 # l += 1
+            # r -= 1
+        return True
 
-        for c in s:
-            if c.isalnum():
-                new += c.lower()
-
-        return new == new[::-1]
+    # helper function
+    def alphaNum(self, c):
+        return (ord('A') <= ord(c) <= ord('Z') or
+                ord('a') <= ord(c) <= ord('z') or
+                ord('0') <= ord(c) <= ord('9'))
+        
