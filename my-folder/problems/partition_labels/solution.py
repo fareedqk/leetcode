2c7faafa3cf4153de463682bae6c2ab0f5476580
Fareed Khan <115370893+fareedqk@users.noreply.github.com>
2024-08-23 16:26:03 +0000

    Sync LeetCode submission - Partition Labels (python3)

---
+++ my-folder/problems/partition_labels/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        last_idx = {}
+        for i, c in enumerate(s):
+            last_idx[c] = i
+        res = []
+        size, end = 0, 0
+
+        for i, c in enumerate(s):
+            size += 1 
+            end = max(end, last_idx[c])
+            if i == end:
+                res.append(size)
+                size = 0
+        return res
