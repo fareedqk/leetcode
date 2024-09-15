f2e394041056001858ba3efdcda881c30853781b
Fareed Khan <115370893+fareedqk@users.noreply.github.com>
2024-09-15 16:52:22 +0000

    Sync LeetCode submission - Permutation in String (python3)

--- my-folder/problems/permutation_in_string/solution.py
+++ my-folder/problems/permutation_in_string/solution.py
@@ -1,18 +1,19 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2): return False
+        lenS1, lenS2 = len(s1), len(s2)
+        if lenS1 > lenS2: return False
 
         charS1 = [0] * 26
         charS2 = [0] * 26
 
-        for i in range(len(s1)):
-            charS1[ord(s1[i]) - ord('a')] += 1
-            charS2[ord(s2[i]) - ord('a')] += 1
-
-        for i in range(len(s1), len(s2)):
+        for x in range(lenS1):
+            charS1[ord(s1[x]) - ord('a')] += 1
+            charS2[ord(s2[x]) - ord('a')] += 1
+        
+        for x in range(lenS1, lenS2):
             if charS1 == charS2: return True
 
-            charS2[ord(s2[i]) - ord('a')] += 1
-            charS2[ord(s2[i - len(s1)]) - ord('a')] -= 1
-
+            charS2[ord(s2[x]) - ord('a')] += 1
+            charS2[ord(s2[x - lenS1]) - ord('a')] -= 1
+        
         return charS1 == charS2
