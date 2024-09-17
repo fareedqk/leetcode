5e852cc329c72cef436d6377957095ac8651d584
Fareed Khan <115370893+fareedqk@users.noreply.github.com>
2024-09-17 10:57:58 +0000

    Sync LeetCode submission - Plus One (python3)

--- my-folder/problems/plus_one/solution.py
+++ my-folder/problems/plus_one/solution.py
@@ -1,19 +1,10 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-    # mul *= 10
-        mul = 1
-        sum = 0
-        for i in digits[::-1]:
-            ans = i * mul
-            # print(i)
-            mul *= 10
-            # print(mul)
-            # sum += i
-            sum = sum + ans
-            # print(sum)
-        plus_one = sum+1
-        # res = [int(res) for res in str(plus_one)]
-        res = [int(res) for res in str(plus_one)]
-        return res
-
+        for i in range(len(digits)-1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            else:
+                digits[i] = 0
         
+        return [1] + digits
