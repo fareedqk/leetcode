400c4ae224015be14c89a47572155110cdcf6d76
Fareed Khan <115370893+fareedqk@users.noreply.github.com>
2024-09-17 09:01:46 +0000

    Sync LeetCode submission - Longest Harmonious Subsequence (python3)

--- my-folder/problems/longest_harmonious_subsequence/solution.py
+++ my-folder/problems/longest_harmonious_subsequence/solution.py
@@ -11,3 +11,14 @@ class Solution:
                     count = longest
                 # count = max(count, hash_map[num] + hash_map[num+1])
         return count
+
+        # nums.sort()
+        # left = 0
+        # max_len = 0
+        # for right in range(len(nums)):
+        #     while nums[right] - nums[left] > 1:
+        #         left += 1
+        #     if nums[right] - nums[left] == 1:
+        #         max_len = max(max_len, right - left + 1)
+        # return max_len
+            
