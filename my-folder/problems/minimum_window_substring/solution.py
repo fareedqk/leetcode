ab7d105e46aa18b41e6a176a86d951c09d02fcd2
Fareed Khan <115370893+fareedqk@users.noreply.github.com>
2024-09-22 14:10:54 +0000

    Sync LeetCode submission - Minimum Window Substring (python3)

--- my-folder/problems/minimum_window_substring/solution.py
+++ my-folder/problems/minimum_window_substring/solution.py
@@ -3,12 +3,14 @@ class Solution:
         if len(s) < len(t) or t == "": # base case
             return ""
         count_t, window = {}, {}
+        
         for c in t:
             count_t[c] = count_t.get(c, 0) + 1
 
         have, need = 0, len(count_t)
         res, res_len = [-1, -1], float('inf')
         l = 0
+
         for r in range(len(s)):
             c = s[r]
             window[c] = window.get(c, 0) + 1
@@ -21,14 +23,10 @@ class Solution:
                 if (r - l + 1) < res_len:
                     res = [l, r]
                     res_len = r - l + 1
-                # pop from left of the window
+                # pop from left from the window
                 window[s[l]] -= 1
                 if s[l] in count_t and window[s[l]] < count_t[s[l]]:
                     have -= 1
                 l += 1
         l, r = res
-        return s[l : r + 1] if res_len != float("inf") else ""
-
-            
-
-        
+        return s[l : r + 1] if res_len != float("inf") else "" 
