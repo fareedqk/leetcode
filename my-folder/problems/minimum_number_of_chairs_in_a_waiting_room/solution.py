796088c821571fa3143d67422cacd723872247e9
Fareed Khan <115370893+fareedqk@users.noreply.github.com>
2024-10-17 13:38:05 +0000

    Sync LeetCode submission - Minimum Number of Chairs in a Waiting Room (python3)

--- my-folder/problems/minimum_number_of_chairs_in_a_waiting_room/solution.py
+++ my-folder/problems/minimum_number_of_chairs_in_a_waiting_room/solution.py
@@ -1,14 +1,15 @@
 class Solution:
     def minimumChairs(self, s: str) -> int:
-        # waiting room
-        person = 0
-        chairs = 0
+        person, chairs = 0, 0
+        
         for i in s:
-            if i == "E":
+            if i == 'E':
                 person += 1
+                # condition --> when we will need chairs?
                 if chairs < person:
                     chairs += 1
-            elif i == "L":
+            elif i == 'L':
                 person -= 1
-
         return chairs
+
+        
