4cf858d91e289b64d1796ded2b0a8d6cf3c0d358
Fareed Khan <115370893+fareedqk@users.noreply.github.com>
2024-10-22 13:14:02 +0000

    Sync LeetCode submission - Isomorphic Strings (python3)

--- my-folder/problems/isomorphic_strings/solution.py
+++ my-folder/problems/isomorphic_strings/solution.py
@@ -1,14 +1,14 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         dict_s, dict_t = {}, {}
-        for i in range(len(s)):
-            c1 = s[i]
-            c2 = t[i]
+
+        for c1, c2 in zip(s, t):
+            # c1 = s[i]
+            # c2 = t[i]
             if (c1 in dict_s and dict_s[c1] != c2) or (c2 in dict_t and dict_t[c2] != c1):
                 return False
 
+            # inserting the elements in the dict
             dict_s[c1] = c2
             dict_t[c2] = c1
         return True
-        
-        
