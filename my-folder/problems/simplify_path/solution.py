34a6bf70c5c12de28271f27f0b14837b7d80064c
Fareed Khan <115370893+fareedqk@users.noreply.github.com>
2024-11-03 16:40:06 +0000

    Sync LeetCode submission - Simplify Path (python3)

---
+++ my-folder/problems/simplify_path/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+        path = path.split('/')
+        # print(path)
+        for i in path:
+            if stack and i == '..':
+                stack.pop()
+            elif i not in ['.', "", ".."]:
+                stack.append(i)
+        return "/" + "/".join(stack)
+            
+
+        
