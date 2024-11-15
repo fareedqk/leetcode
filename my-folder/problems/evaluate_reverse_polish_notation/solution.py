77b7e4d7f8d50810da6c27ed1fe6e76278bc7078
Fareed Khan <115370893+fareedqk@users.noreply.github.com>
2024-11-15 13:28:39 +0000

    Sync LeetCode submission - Evaluate Reverse Polish Notation (python3)

--- my-folder/problems/evaluate_reverse_polish_notation/solution.py
+++ my-folder/problems/evaluate_reverse_polish_notation/solution.py
@@ -4,15 +4,14 @@ class Solution:
         for char in tokens:
             if char == '+':
                 stack.append(stack.pop() + stack.pop())
+            elif char == "*":
+                stack.append(stack.pop() * stack.pop())
             elif char == '-':
                 a, b = stack.pop(), stack.pop()
                 stack.append(b - a)
-            elif char == '*':
-                stack.append(stack.pop() * stack.pop())
             elif char == '/':
                 a, b = stack.pop(), stack.pop()
                 stack.append(int(b / a))
-            else:
+            else: 
                 stack.append(int(char))
         return stack[0]
-        
