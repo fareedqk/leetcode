a515ea207e4abcc6a0de045dab98f5dd2153418d
Fareed Khan <115370893+fareedqk@users.noreply.github.com>
2025-05-13 07:43:44 +0000

    Sync LeetCode submission - Add Digits (python3)

--- my-folder/problems/add_digits/solution.py
+++ my-folder/problems/add_digits/solution.py
@@ -1,10 +1,13 @@
 class Solution:
     def addDigits(self, num: int) -> int:
-        if num == 0:
-            return 0
-        if num % 9 == 0:
-            return 9
-        else:
-            return num % 9
+        # if num == 0:
+        #     return 0
+        # if num % 9 == 0:
+        #     return 9
+        # else:
+        #     return num % 9
 
+        while num >= 10:
+            num = sum(int(digit) for digit in str(num))
+        return num
         
